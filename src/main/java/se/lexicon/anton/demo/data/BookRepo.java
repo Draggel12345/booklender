5e104cbbae32ff218693e3be86a253fbd25d0f73
Draggel12345 <anton.edholm@hotmail.com>
2019-12-02 10:54:18 +0100

    Added CRUD repos.

--- src/main/java/se/lexicon/anton/demo/data/BookRepo.java
+++ src/main/java/se/lexicon/anton/demo/data/BookRepo.java
@@ -1,9 +1,18 @@
 package se.lexicon.anton.demo.data;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import se.lexicon.anton.demo.model.Book;
 
 public interface BookRepo extends CrudRepository<Book, Integer>{
 
+	Optional<Book> findByBookId(int bookId);
+	List<Book> findByTitleStartsWithIgnoreCase(String title);
+	List<Book> findByAvailable(boolean available);
+	List<Book> findByReserved(boolean reserved);
+	List<Book> findAll();
+	
 }
--- src/main/java/se/lexicon/anton/demo/data/LibraryUserRepo.java
+++ src/main/java/se/lexicon/anton/demo/data/LibraryUserRepo.java
@@ -1,9 +1,15 @@
 package se.lexicon.anton.demo.data;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import se.lexicon.anton.demo.model.LibraryUser;
 
 public interface LibraryUserRepo extends CrudRepository<LibraryUser, Integer> {
 
+	Optional<LibraryUser> findByUserId(int userId);
+	Optional<LibraryUser> findByEmailStartsWithIgnoreCase(String email);
+	List<LibraryUser> findAll();
 }
